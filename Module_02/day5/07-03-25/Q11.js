307411ce85db608c9ec7bf6302020d3ebcdda513
Bharath Govindula <bharathraj1564@gmail.com>
2025-03-07 19:46:46 +0530

    closures in js

---
+++ Module_02/day5/07-03-25/Q11.js
@@ -0,0 +1,18 @@
+// Create a simple function outerFunction that defines a variable message within its scope.
+//  Return an inner function that logs the message when invoked.
+//  Demonstrate the concept of closure by invoking the outer function and storing the result in a variable.
+//  Call the stored function and ensure that it logs the correct message even after the outer function has completed its execution.
+
+
+
+function outerFunction() {
+    let message = "Hello from closure!"; 
+  
+    return function innerFunction() { // Inner function (closure)
+      console.log(message); // Accessing 'message' from outer scope
+    };
+  }
+  
+  const storedFunction = outerFunction(); 
+  storedFunction(); 
+  
---
+++ Module_02/day5/07-03-25/Q13.js
@@ -0,0 +1,54 @@
+// Create a closure that represents a bank account.
+//  Implement a function createBankAccount that initializes an account with a balance (input by the user) and returns methods to deposit, withdraw, and getBalance. Ensure that the balance cannot be accessed or modified directly from outside the closure.
+
+// deposit(amount): Increases the balance by the given amount.
+// withdraw(amount): Decreases the balance by the given amount, if there are sufficient funds.
+// getBalance(): Returns the current balance.
+// Example:
+
+// const account = createBankAccount(100);
+
+// console.log(account.deposit(50)); // Output: 150
+
+// console.log(account.withdraw(30)); // Output: 120
+
+// console.log(account.getBalance()); // Output: 120
+
+
+
+
+function createBankAccount(initialBalance) {
+    let balance = initialBalance;
+
+    return {
+        deposit: function(amount) {
+            if (amount > 0) {
+                balance += amount;
+                return balance;
+            } else {
+                return "Deposit amount must be greater than zero.";
+            }
+        },
+        withdraw: function(amount) {
+            if (amount > balance) {
+                return "Insufficient funds.";
+            } else if (amount > 0) {
+                balance -= amount;
+                return balance;
+            } else {
+                return "Withdrawal amount must be greater than zero.";
+            }
+        },
+        getBalance: function() {
+            return balance;
+        }
+    };
+}
+
+
+const account = createBankAccount(100);
+
+console.log(account.deposit(50)); // Output: 150
+console.log(account.withdraw(30)); // Output: 120
+console.log(account.getBalance()); // Output: 120
+
